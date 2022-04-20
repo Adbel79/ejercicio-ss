7c8c543c8aacfa6b2c5eb5c4b8c97ad15aa46052
Adbel79 <adbel79@gmail.com>
2022-04-19 22:47:00 -0500

    cambios en el endpoint

--- src/main/java/mx/uv/t4is/agendabd/agendabdEndPoint.java
+++ src/main/java/mx/uv/t4is/agendabd/agendabdEndPoint.java
@@ -21,16 +21,20 @@ public class agendabdEndPoint {
 
     @Autowired
     Iagendadores iagendadores;
-
+     //se crea el metodo donde generaremos el contacto de la agenda 
     @PayloadRoot(localPart = "CrearContactoRequest" ,namespace = "https://t4is.uv.mx/agendabd")
     @ResponsePayload
     public CrearContactoResponse CrearContacto(@RequestPayload CrearContactoRequest peticion) {
+        //se crea una respuesta  
         CrearContactoResponse respuesta = new CrearContactoResponse();
         respuesta.setRespuesta("contacto guardado exitosamente! ");
+        //se hace un objeto del tipo agendadores 
         Agendadores agenda = new Agendadores();
+        //se obtienen el nombre, numero y corrreo
         agenda.setNombre(peticion.getNombre());
         agenda.setNumero(peticion.getNumero());
         agenda.setCorreo(peticion.getCorreo());
+        //se guarda lo que hay 
         iagendadores.save(agenda);
         return respuesta;  
     }
@@ -39,26 +43,34 @@ public class agendabdEndPoint {
     @ResponsePayload
     public BuscarContactosResponse buscarContactos(){
         BuscarContactosResponse respuesta = new BuscarContactosResponse();
+        //se genera una lista 
         Iterable<Agendadores> lista = iagendadores.findAll();
-        
-        for (Agendadores saludo : lista) {
+        //se recorre la lista de los contactos
+        for (Agendadores agenda : lista) {
             BuscarContactosResponse.Contactos e = new BuscarContactosResponse.Contactos();
-            e.setNombre(saludo.getNombre());
-            e.setId(saludo.getId());
-            e.setNumero(saludo.getNumero());
-            e.setCorreo(saludo.getCorreo());
+            //se obtienen los datos
+            e.setNombre(agenda.getNombre());
+            e.setId(agenda.getId());
+            e.setNumero(agenda.getNumero());
+            e.setCorreo(agenda.getCorreo());
+            //los guarda encontactos
             respuesta.getContactos().add(e);
         }
+        //regresa la respuesta 
         return respuesta;
     }
 
     @PayloadRoot(localPart = "EliminarContactoRequest", namespace = "https://t4is.uv.mx/agendabd")
     @ResponsePayload
     public EliminarContactoResponse eliminarContacto(@RequestPayload EliminarContactoRequest peticion){
+        //se hace la respuesta de tipo eliminar contactos
         EliminarContactoResponse respuesta = new EliminarContactoResponse();
+        //se obtiene el id del contacto a borrar y se elimina 
         iagendadores.deleteById(peticion.getId());
         respuesta.setRespuesta("contacto eliminado");
+        //regresa la respuesta
         return respuesta;
     }
 
 }
+
